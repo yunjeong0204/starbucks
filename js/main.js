cfb86f8072f12f8c701aa124e90a48def42c20ac
yunjeong0204 <dua0204@naver.com>
2023-04-14 18:30:34 +0900

    starbucks만들기

--- js/main.js
+++ js/main.js
@@ -31,9 +31,43 @@ window.addEventListener('scroll', function () {
   // 만약 y축으로 스크롤 한 수치가 500을 초과하면 배지 요소를 숨기고 그렇지 않으면 다시 보이기
   if (window.scrollY > 500) {
     // 배지 요소 숨기기
-    badgeEl.style.display = "none";
-  } else { //배지 요소 보이기
-    badgeEl.style.display = "block";
-  }
+    // badgeEl.style.display = "none";
 
+    //gsap.to(요소, 지속시간, 옵션: {}) 메소드: css 속성을 통해 애니메이션 처리
+    gsap.to(badgeEl, 0.6, {
+      opacity: 0,
+      display: 'none'
+    });
+  } else { //배지 요소 보이기
+    // badgeEl.style.display = "block";
+
+    gsap.to(badgeEl, 0.6, {
+      opacity: 1,
+      display: 'block'
+    });
+  }
+});
+
+// 순차적으로 VISUAL 섹션 내 요소 보이기
+// 나타날 요소(.fade-in)들을 찾기
+const fadeEls = document.querySelectorAll('.visual .fade-in');
+
+// 요소들을 하나씩 반복해서 처리
+fadeEls.forEach(function (fadeEl, index) {
+  //gsap.to(요소, 지속시간, 옵션: {}) 메소드: css 속성을 통해 애니메이션 처리
+  gsap.to(fadeEl, 1, {
+    // delay: 몇 초 뒤에 실행될 것인가
+    delay: (index + 1) * 0.7, //0.7 1.4 2.1 2.8
+    opacity: 1
+  });
+
+});
+
+//공지사항 수직 슬라이드 기능 작성
+// new 키워드로 swiper 객체를 생성 => 슬라이드 기능을 생성
+// new Swiper(선택자, 옵션: {});
+new Swiper('.notice .swiper', {
+  direction: 'vertical', //수직 슬라이드
+  loop: true, //반복 재생 여부
+  autoplay: true, //자동 재생 여부
 });
