dbff2da3d000c6bb30db89eaf1160a1c9be4ed9e
yunjeong0204 <dua0204@naver.com>
2023-04-19 17:28:46 +0900

    스타벅스 만들기

--- js/main.js
+++ js/main.js
@@ -23,6 +23,14 @@ searchinputEl.addEventListener('blur', function () {
 // 스크롤 시 전역 배지(고정 배지) 숨기기
 const badgeEl = document.querySelector('header .badges');
 
+// 상단으로 이동 버튼 제어
+const toTopEl = document.querySelector('#to-top');
+toTopEl.addEventListener('click', function () {
+  gsap.to(window, 0.6, {
+    scrollTo: 0 //페이지의 0px 지점(최상단)으로 이동, scrollToPlugin을 연결해야 사용 가능한 옵션
+  });
+});
+
 // window: 브라우저 창 객체
 window.addEventListener('scroll', function () {
   // y축으로 얼마나 스크롤 했는지에 대한 수치
@@ -38,12 +46,24 @@ window.addEventListener('scroll', function () {
       opacity: 0,
       display: 'none'
     });
-  } else { //배지 요소 보이기
-    // badgeEl.style.display = "block";
 
+    // 상단으로 이동 버튼 보이기!
+    gsap.to(toTopEl, 0.6, {
+      opacity: 1,
+      x: 0 //x축 0px 지점으로 이동
+    });
+  } else {
+    //배지 요소 보이기
+    // badgeEl.style.display = "block";
     gsap.to(badgeEl, 0.6, {
       opacity: 1,
       display: 'block'
+    });
+
+    // 상단으로 이동 버튼 숨기기
+    gsap.to(badgeEl, 0.6, {
+      opacity: 1,
+      x: 100  //x축 100px 지점으로 이동
     });
   }
 });
@@ -155,7 +175,22 @@ new Swiper('.awards .swiper', {
   },
 });
 
+// ScrollMagic 사용
+const spyEls = document.querySelectorAll('section.scroll-spy');
+spyEls.forEach(function (spyEl) {
+  new ScrollMagic
+    .Scene({ //감시할 장면(Scene) 추가 및 옵션 지정
+      triggerElement: spyEl, //보여짐 여부를 감시할 요소를 지정
+      triggerHook: 0.8 //화면의 80% 지점에서 보여짐 여부 감시(0~1사이 지정)
+    })
+    .setClassToggle(spyEl, 'show') //요소가 화면에 보이면 show클래스 추가
+    .addTo(new ScrollMagic.Controller()); //컨트롤러에 장면을 할당(필수임) - 라이브러리에서 지정한 문법
+});
 
+//현재 연도 표시
+//날짜 정보를 가진 JS의 Date 객체를 활용 
+const thisYear = document.querySelector('.this-year');
+thisYear.textContent = new Date().getFullYear();//현재 연도의 정보가 숫자 데이터로 반환됨
 
 
 
